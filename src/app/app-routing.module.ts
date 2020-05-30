b8cb37228c41253f387c6d731a65b708d46b3b45
jhonnatthan <jhonnatthans@outlook.com>
2020-05-30 19:35:02 -0300

    [ADD] - Estrutura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { CotacaoComponent } from './cotacao/cotacao.component';
+import { EquipeComponent } from './equipe/equipe.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'price', component: CotacaoComponent },
+  { path: 'history', component: HistoricoComponent },
+  { path: 'team', component: EquipeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EquipeComponent } from './equipe/equipe.component';
+import { CotacaoComponent } from './cotacao/cotacao.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+registerLocaleData(pt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EquipeComponent,
+    CotacaoComponent,
+    HistoricoComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
